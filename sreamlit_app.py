ef08f9287866bc26140b85f9c54597f7170310a0
medtestdata <109354373+medtestdata@users.noreply.github.com>
2022-07-15 16:38:57 +0200

    Update sreamlit_app.py
    Breackfast Menu Add

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
-streamlit.title('Parents New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach & Rocket smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🍲 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
