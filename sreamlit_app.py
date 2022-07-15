6a1f21191cd98661ca3966598370fa67962a2e31
medtestdata <109354373+medtestdata@users.noreply.github.com>
2022-07-15 16:03:37 +0200

    Update sreamlit_app.py
    Breakfast basic Menu

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('Parents New Healthy Diner')
+
+streamlit.text('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale, spinach & Rocket smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
