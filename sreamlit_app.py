50a7aaeff74f1ba0c0226c54c955fcaea79f779e
medtestdata <109354373+medtestdata@users.noreply.github.com>
2022-07-19 17:25:16 +0200

    Update sreamlit_app.py
    fix

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -28,4 +28,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalize
+streamlit.dataframe(fruityvice_normalize)
