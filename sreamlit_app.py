62d35d427f5edf9dcd9571d8d25bb367635067c0
medtestdata <109354373+medtestdata@users.noreply.github.com>
2022-07-20 15:08:30 +0200

    Update sreamlit_app.py
    Fetch and show fruit_load_list with snowflake_connector

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -33,3 +33,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
